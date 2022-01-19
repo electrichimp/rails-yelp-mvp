beb2d82f6a36ac078f8bc6a4143682ccf7a5b209
JUAN MANUEL GOMEZ <jumagom@gmail.com>
2022-01-19 14:43:41 -0500

    ya ta

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"].freeze
+
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name,
+                    address: Faker::Address.street_address,
+                    phone_number: Faker::PhoneNumber.phone_number,
+                    category: CATEGORIES.sample)
+end
